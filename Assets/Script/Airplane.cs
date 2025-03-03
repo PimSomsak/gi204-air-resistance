991585768b21664d77c5487f37ccdb6b08602481
PimSomsak <pym.somsak@gmail.com>
2025-03-03 11:00:39 +0700

    Complete Airplane

--- Assets/Script/Airplane.cs
+++ Assets/Script/Airplane.cs
@@ -7,21 +7,38 @@ public class Airplane : MonoBehaviour
     [SerializeField] float liftBooster = 0.5f;
     [SerializeField] float drag = 0.001f;
     [SerializeField] float angularDrag = 0.001f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] float yawPower = 50f;
+    [SerializeField] float pitchPower = 50f;
+    [SerializeField] float rollPower = 30f;
+   
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
+
     private void FixedUpdate()
     {
+        float yaw = Input.GetAxis("Horizontal") * yawPower;
+        float pitch = Input.GetAxis("Vertical") * pitchPower;
+        float roll = Input.GetAxis("Roll") * rollPower;
+
+        rb.AddTorque(transform.up * yaw);
+        rb.AddTorque(transform.right * pitch);
+        rb.AddTorque(transform.forward * roll);
+
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddForce(transform.forward * enginePower);
 
             Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
             rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+            rb.linearDamping = rb.linearVelocity.magnitude * drag;
+            rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
         }
     }
 }
